d39f153a81500ea11b6ee090bb4bb464c0d0f43e
Ajibola Sadiq <ajibola.sadiq@cognitivelabs.co>
2023-01-08 16:46:08 +0100

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+ const[value, setValue] = useState<number>()
+  return (
+    <p>
+      hello world
+    </p>
+  );
+}
+
+export default App;
