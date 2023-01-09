7da2a09f39499f459977d829c99b30d7583c2edd
Ajibola Sadiq <ajibola.sadiq@cognitivelabs.co>
2023-01-09 13:07:58 +0100

    signup page

--- src/hooks/hooks.tsx
+++ src/hooks/hooks.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
-import { User } from "../types/user"
+import { User, UserLogin } from "../types/user"
 
 export const usePostRequest = () => {
     const navigate = useNavigate()
@@ -8,6 +8,22 @@ export const usePostRequest = () => {
         axios.post(url, req)
             .then((response) => {
                 navigate("/signin")
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+    return postUser
+}
+
+
+export const useLoginRequest = () => {
+    const navigate = useNavigate()
+    const postUser = (url: string, req: UserLogin) => {
+        axios.post(url, req)
+            .then((response) => {
+               if(response){
+                navigate("/")
+               }
             }).catch((error) => {
                 console.log(error)
             })
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { useLoginRequest, usePostRequest } from '../hooks/hooks'
+import { User, UserLogin } from '../types/user'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    let [value, setValue] = useState<UserLogin>({email: "", password: "" })
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        setValue({ ...value, [e.target.name]: e.target.value })
+    }
+    const postUser = useLoginRequest()
+    const handleSubmit =(e:FormEvent) =>{
+        e.preventDefault()
+        postUser("http://localhost:8080/api/v1/user/login", value)
+    }
+    return (
+        <div className='w-[100wh] h-[100vh] flex justify-center items-center bg-[#efefef]'>
+            <form action="" onSubmit={(e)=>handleSubmit(e)} className='w-[500px] h-[280px] py-6 px-4 flex justify-between items-start flex-col bg-white rounded-lg'>
+                <h1 className='text-purple-600 font-extrabold text-xl'>Sign In</h1>
+                <input type="email" placeholder='Enter email' onChange={(e) => handleChange(e)}  value={value.email} name='email' className='h-10 w-full bg-purple-100 p-3 rounded-md outline-none border-none text-gray-500' />
+                <input type="password" placeholder='Enter password' onChange={(e) => handleChange(e)}  value={value.password} name='password' className='h-10 w-full bg-purple-100 p-3 rounded-md outline-none border-none text-gray-500' />
+                <button type='submit' className='w-full bg-purple-400 text-white text-lg rounded-normal p-2 rounded-md outline-none border-none shadow-2xl font-bold'>submit</button>
+                <small className='text-gray-500'>Forgot your password? <span className='font-bold'>Click here</span></small>
+            </form>
+        </div>
+    )
 }
 
 export default Login
